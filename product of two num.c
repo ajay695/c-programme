6bacf6aa7965464228a33c27103987c50d4bde00
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-04-30 18:03:10 +0545

    Update product of two num.c

--- product of two num.c
+++ product of two num.c
@@ -4,7 +4,7 @@ void main()
 {
 int a=10, b=8,product;
 product=a*b;
-printf("The product of %d and %d is %d.",a,b,product.);
+printf("The product of %d and %d is %d.",a,b,product);
     
     return(0);
 }
