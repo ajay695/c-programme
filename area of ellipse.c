ec02eb1416be7eea6932872e60efbc2c19785f47
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-04-30 18:50:59 +0545

    Add files via upload

---
+++ area of ellipse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#define pi 3.14
+
+      void main()
+{
+        float minor=4,major=6,volume;
+      //calculating area of an ellipse
+        volume=pi*minor*major;
+      
+           printf("volume of an ellipse is %.2f",volume); 
+    
+    getch();
+    }
