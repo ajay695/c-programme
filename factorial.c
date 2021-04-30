2424374320c096e5cc24670acf90521f2e7c8ba0
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-04-30 19:49:03 +0545

    Add files via upload

--- factorial.c
+++ factorial.c
@@ -1,2 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int num,i,fact=1;
+     printf("Enter factorial number:");
+     scanf("%d",&num);
+  for (i=num;i>0;i=i-1
 
-    
+  fact=fact*i;
+printf("\nThe factorial of %d is %d", num,fact);
+
+getch();
+}
