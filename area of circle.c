4f132581712d7d7548a39530ab9be42635df4d97
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-04-30 18:44:24 +0545

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#define pi 3.14
+
+      void main()
+{
+        float radius=5,area;
+      //calculating area
+      area=pi*radius*radius;
+      
+           printf("Area of circle is %.2f",area); 
+    
+    getch();
+    }
---
+++ volume of cuboid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+      void main()
+{
+        float h=10,w=12,d=8,volume;
+      //calculating volume
+      volume=d*w*h;
+      
+           printf("volume of cuboid is %.2f",volume); 
+    
+    getch();
+    }
