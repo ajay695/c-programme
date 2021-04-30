bdc8200b302f5be4b84e113bf8c03a4a84754e93
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-04-30 18:57:00 +0545

    Add files via upload

---
+++ simple intrest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+     void main()
+{ 
+     float P=4000,T=2,R=5.5,I;
+     //calculating simple intrest
+     I=P*T*R/100;
+     
+        printf("Simple Intrest=%.2f",I);
+    getch();
+}
