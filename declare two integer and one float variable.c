69b63dcee5245b6a60897fc2fdf5135c5739e2a1
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-04-30 19:14:56 +0545

    Add files via upload

---
+++ declare two integer and one float variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+     void main()
+{ 
+     int a=8,b=4;
+     float c=2.5;
+     
+          printf("The value of two integer are %d,%d and one float variable is %.2f",a,b,c);
+    getch();
+}
