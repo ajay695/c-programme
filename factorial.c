e4cba75f4143496844cf4137d3059d74adee2d1f
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-04-30 19:48:07 +0545

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,14 +1,2 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
- long int f=1;
-      int i;
-  for (i=1;i<=5;i++)
-{
-  f=f*i;
-printf("\nThe factorial of %d is %d", i,f);
-}
-getch();
-}
+
     
