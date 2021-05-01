09e219b68251c1d894219a7d5a00c3eefa163405
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-05-01 17:12:45 +0545

    Add files via upload

---
+++ 28.day name using switch case.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+   int main()
+{
+      int week;
+      
+      //Input week number from user
+      printf("Enter a week number(1-7):");
+      scanf("%d",&week);
+      
+      switch(week)
+      {
+          case 1:
+                 printf("Sunday");
+                 break;
+          case 2:
+                 printf("Monday");
+                 break;
+          case 3:
+                 printf("Tuesday");
+                 break;
+          case 4:
+                 printf("Wednesday");
+                 break;
+          case 5:
+                 printf("Thursday");
+                 break;
+          case 6:
+                 printf("Friday");
+                 break;
+          case 7:
+                 printf("Saturday");
+                 break;
+        default:
+                 printf("Invalid input! Pleas enter the week number between 1-7.");
+                 
+          
+      }
+      return 0;
+}
