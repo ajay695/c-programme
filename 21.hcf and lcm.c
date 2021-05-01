0976c43f403f64894d520ba45b237da6c9000bed
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-05-01 20:44:21 +0545

    Update 21.hcf and lcm.c

--- 21.hcf and lcm.c
+++ 21.hcf and lcm.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
 
+     void main()
+{
+	 int n1, n2, hcf, lcm, i;
+	 printf("Enter 1st and 2nd number: ");
+	 scanf("%d,%d", &n1,&n2);
+	 // Finding HCF 
+	 for(i=1; i<=n1; i++)
+	 {
+		  if(n1%i==0 && n2%i==0)
+		  {
+		   	hcf = i;
+		  }
+	 }
+	 //Finding lcm 
+	 lcm = (n1 * n2)/hcf;
+	 printf("HCF = %d and lcm = %d", hcf, lcm);
+	 
+	 return ;
+}
