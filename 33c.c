2f3c765c5d302af919243f49dd97dc177db0eea5
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-05-01 19:55:08 +0545

    Update 33c.c

--- 33c.c
+++ 33c.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+#include <conio.h>
+      void main()
+{
+   int i, j, n=5;
+   
+   for (i = 1; i <= n; ++i)
+   {
+      for (j = 1; j <= i; ++j) {
+         printf("%d ", j);
+      }
+      printf("\n");
+   }
+   return ;
+}
