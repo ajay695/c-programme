cacb3380007cea08e11363eaa99d86ac96ed6a4a
ajay695 <72080374+ajay695@users.noreply.github.com>
2021-05-03 13:46:34 +0545

    Update 14.forward and rev.c

--- 14.forward and rev.c
+++ 14.forward and rev.c
@@ -9,3 +9,25 @@ printf("The forward order:%d,%d,%d\n",a,b,c);
 printf("The reverse order:%d,%d,%d",c,b,a);
 getch();
 }
+
+//using while loop
+#include <stdio.h>
+ 
+void main()
+{
+  int n, reverse;
+ 
+  printf("Enter a number to reverse\n");
+  scanf("%d", &n);
+  printf("forward number is  %d\n",n);
+  while (n != 0)
+  {
+    reverse = reverse * 10;
+    reverse = reverse + n%10;
+    n = n/10;
+  }
+
+  printf("Reverse number is  %d\n", reverse);
+ 
+  return ;
+}
